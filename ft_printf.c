c91a84a39e92e699c2e94a3c4b34db4342b68129
thk2b <theokobb@gmail.com>
2018-10-03 13:26:03 -0700

    organize functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,58 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/02 20:39:48 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/02 20:53:51 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/03 13:25:16 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdarg.h>
+
+int	ft_vsnprintf(char *s, size_t len, const char *fmt, va_list ap)
+{
+
+}
+
+int	ft_asprintf(char **s, const char *fmt, ...)
+{
+	
+}
+
+int	ft_snprintf(char *s, size_t len, const char *fmt, ...)
+{
+
+}
+
+int	ft_vsprintf(char *s, const char *fmt, va_list ap)
+{
+
+}
+
+int	ft_sprintf(char *s, const char *fmt, ...)
+{
+
+}
+
+int	ft_vdprintf(int fd, const char *fmt, va_list ap)
+{
+	
+}
+
+int	ft_dprintf(int fd, const char *fmt, ...)
+{
+
+}
+
+int	ft_dprintf(int fd, const char *fmt, ...)
+{
+	va_list ap;
+	int len;
+
+	va_start(ap, fmt);
+	len = ft_vdprintf(fd, fmt, ap);
+	va_end(ap);
+	return len;
+}
 
 int	ft_printf(const char *fmt, ...)
 {
--- ft_vsnprintf.c
+++ ft_vsnprintf.c
@@ -1,25 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_dprintf.c                                       :+:      :+:    :+:   */
+/*   ft_vsnprintf.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/02 20:39:34 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/02 20:53:14 by tkobb            ###   ########.fr       */
+/*   Created: 2018/10/03 13:18:13 by tkobb             #+#    #+#             */
+/*   Updated: 2018/10/03 13:18:24 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdarg.h>
 
-int	ft_dprintf(int fd, const char **fmt, ...)
+int	vsnprintf(char *s, size_t len, const char *fmt, va_list args)
 {
-	va_list ap;
-	int len;
-
-	va_start(ap, fmt);
-	len = ft_vdprintf(fd, fmt, ap);
-	va_end(ap);
-	return len;
+	
 }
