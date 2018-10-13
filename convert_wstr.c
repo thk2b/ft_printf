c13477717898ac5ec841b07675296962a83f7221
thk2b <theokobb@gmail.com>
2018-10-13 13:38:51 -0700

    !

--- convert_wstr.c
+++ convert_wstr.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/10 19:11:34 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/13 13:35:02 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/13 13:36:27 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ char	*ft_wstrncpy(char *dst, const wchar_t *src, size_t len)
 	d = dst;
 	while (len--)
 		if (*src)
-			*dst++ = 0;
-			// *dst++ = (char)*src++;
+			*dst++ = (char)*src++;
 		else
 			*dst++ = '\0';
 	*dst = '\0';
