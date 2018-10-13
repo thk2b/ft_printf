2000909a5bb52a5f4ce95af052dad9f149a84faa
thk2b <theokobb@gmail.com>
2018-10-13 14:16:49 -0700

    stars

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/04 10:40:33 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/13 12:27:37 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/13 14:16:20 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,16 @@ static void	reset_directive(t_directive *d)
 	d->width = 0;
 }
 
-static int	parse_width(t_directive *d, const char *fmt)
+static int	parse_width(t_directive *d, const char *fmt, va_list ap)
 {
 	int len;
 
 	len = 0;
+	if (fmt[len] == '*')
+	{
+		d->width = va_arg(ap, int);
+		return (1);
+	}
 	if (ft_isdigit(*fmt))
 	{
 		d->width = ft_atoi(fmt);
@@ -38,13 +43,18 @@ static int	parse_width(t_directive *d, const char *fmt)
 	return (0);
 }
 
-static int	parse_precision(t_directive *d, const char *fmt)
+static int	parse_precision(t_directive *d, const char *fmt, va_list ap)
 {
 	int len;
 
 	if (*fmt != '.')
 		return (0);
 	len = 1;
+	if (fmt[len] == '*')
+	{
+		d->precision = va_arg(ap, int);
+		return (2);
+	}
 	if (ft_isdigit(fmt[len]))
 	{
 		d->precision = ft_atoi(fmt + len);
@@ -72,15 +82,15 @@ static int	parse_convertion(t_directive *d, const char *fmt)
 	return (1);
 }
 
-int			parse(t_directive *d, const char *fmt)
+int			parse(t_directive *d, const char *fmt, va_list ap)
 {
 	int len;
 
 	reset_directive(d);
 	len = 0;
 	len += parse_flags(d, fmt + len);
-	len += parse_width(d, fmt + len);
-	len += parse_precision(d, fmt + len);
+	len += parse_width(d, fmt + len, ap);
+	len += parse_precision(d, fmt + len, ap);
 	len += parse_length(d, fmt + len);
 	len += parse_convertion(d, fmt + len);
 	return (len);
